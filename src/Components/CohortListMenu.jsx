f3b2d939c94e305d255ad66cf8fe69766fd92bb2
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-08-04 08:46:49 -0400

    Change 'h3' back to 'li'.

--- src/Components/CohortListMenu.jsx
+++ src/Components/CohortListMenu.jsx
@@ -19,9 +19,9 @@ export default function CohortListMenu({ studentData, setCohortName }) {
 		<div className='cohort-list'>
 			<h2>Choose a Class by Start Date</h2>
 
-			<h3 onClick={() => setCohortName('All Students')}>All Students</h3>
-
 			<ul>
+				<li onClick={() => setCohortName('All Students')}>All Students</li>
+
 				{sortedCohortList.map((cohortName) => (
 					<li
 						key={crypto.randomUUID()}
